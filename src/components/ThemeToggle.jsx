dcbcf9a44cff2d220357dc8a8cd4f71f2db08f19
oclaudiodev <7claudiosouza@gmail.com>
2026-03-28 18:13:19 -0300

    modeLight

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import "./ThemeToggle.css";
 
 export default function ThemeToggle({ dark, onToggle }) {
   return (
-    <button className="theme-toggle" onClick={onToggle} title="Alternar tema">
+    <button className="theme-toggle" onClick={onToggle} title={dark ? "Modo Claro" : "Modo Dark"}>
       {dark ? <MdLightMode size={18} /> : <MdDarkMode size={18} />}
     </button>
   );
