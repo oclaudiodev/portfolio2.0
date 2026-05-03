35c51f91425afb286fe952d5aa2b031d6fa4eccb
oclaudiodev <7claudiosouza@gmail.com>
2026-05-03 14:33:35 -0300

    removendoCV

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -117,18 +117,7 @@ export default function Hero() {
 
           <div className={cx("hero-btns")} style={{ animationDelay: "0.9s" }}>
             <a href="#projects" className="btn-neon">VER PROJETOS</a>
-            <a
-              href="/JoséClaudio_curriculo.pdf"
-              download="JoséClaudio_curriculo.pdf"
-              className="btn-cv"
-            >
-              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
-                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-                <polyline points="7 10 12 15 17 10"/>
-                <line x1="12" y1="15" x2="12" y2="3"/>
-              </svg>
-              BAIXAR CV
-            </a>
+            
           </div>
 
           <div
